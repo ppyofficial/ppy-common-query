95400d6b7f97844f5de60e5975af2dd1217ed6c7
冯宇鹏 <fengyupeng@bonc.com.cn>
2024-01-15 14:23:19 +0800

    修改配置文件

--- ppy-common-query-app/src/main/java/io/github/ppy/app/StarterApplication.java
+++ ppy-common-query-app/src/main/java/io/github/ppy/app/StarterApplication.java
@@ -1,9 +1,16 @@
 package io.github.ppy.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * {@code Description}  : Query启动类
  * {@code @Author} : 冯宇鹏
  * {@code @Date:} : 2024-01-04 14:33:54
  */
+@SpringBootApplication
 public class StarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterApplication.class,args);
+    }
 }
